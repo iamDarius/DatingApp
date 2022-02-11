1e070699636d15dc5cd72ac3cf1287d0ed3881e6
Darius Moore <dmoore@costar.com>
2022-02-11 14:42:59 -0500

    Adding validation for register DTOs

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Username {get; set; }
         public string Password { get; set; }
 
