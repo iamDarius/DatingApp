f52126b4b04b100a872138dffcae66714daca8dd
Darius Moore <iamdariusmoore@gmail.com>
2022-02-16 12:38:39 -0500

    shared module added

---
+++ client/src/app/modules/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToastrModule } from 'ngx-toastr';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+/* using this shared module for 3rd party modules */
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+    }),
+  ],
+  exports: [CommonModule, BsDropdownModule, ToastrModule],
+})
+export class SharedModule {}
